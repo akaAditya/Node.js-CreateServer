3f19497a9a981afafb01a3a97421c0cf9faca212
akaaditya <mymail.adityapatel@gmail.com>
2024-03-03 02:37:30 +0530

    second commit basic routing with html

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
 const http = require('http');
 
 const server = http.createServer((req, res)=>{
-    console.log('Aditya')
+    const url = req.url;
+
+    if(url === '/'){
+        res.write('<html>')
+        res.write('<head><title></title></head>')
+        res.write('<body><h1>Welcome to my Node.js HomePage</h1></body>')
+        res.write('</html>')
+        return res.end();
+    }
+    else if(url === '/home'){
+        res.write('<html>')
+        res.write('<head><title></title></head>')
+        res.write('<body><h1>Welcome Home</h1></body>')
+        res.write('</html>')
+        return res.end();
+    }
+    else if(url === '/about'){
+        res.write('<html>')
+        res.write('<head><title></title></head>')
+        res.write('<body><h1>Welcome to About Us page</h1></body>')
+        res.write('</html>')
+        return res.end();
+    }
+    else if(url === '/node'){
+        res.write('<html>')
+        res.write('<head><title></title></head>')
+        res.write('<body><h1>Welcome to Node.js project</h1></body>')
+        res.write('</html>')
+        return res.end();
+    }
+
 })
 
 server.listen(4000);
